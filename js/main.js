cf3f993408872d6f8ce0bca2f1e85acb3ceb9926
jcomins <jcomins000@gmail.com>
2016-07-18 14:37:55 -0700

    added two features and a count of current evolves

--- js/main.js
+++ js/main.js
@@ -6,13 +6,46 @@ $(function() {
         	do_melt = !do_melt;
             return old === 'Transfer evolutions' ? "Don't transfer evolutions" : 'Transfer evolutions';
         });
+        $('input').trigger('input');
     });
+    $('.switch').click( function() {  
+	    $(this).addClass('active');
+	    $('.switch').not(this).removeClass('active');
+	    evolve_rate = parseInt($(this).text());
+	    $('input').trigger('input');
+	}).focus( function() {
+		$(this).click();
+	});
+	$('#catch').click( function() {
+		$('#candies').val( function() {
+			var candies = ($('#candies').val().length != 0) ? parseInt($('#candies').val(), 10) : 0;
+			return candies + 3;
+		});
+		$('#pokemon').val( function() {
+			var pokemon = ($('#pokemon').val().length != 0) ? parseInt($('#pokemon').val(), 10) : 0;
+			return ++pokemon;
+		});
+		$('input').trigger('input');
+	});
+	$('#melt').click( function() {
+		var candies = ($('#candies').val().length != 0) ? parseInt($('#candies').val(), 10) : 0;
+		var pokemon = ($('#pokemon').val().length != 0) ? parseInt($('#pokemon').val(), 10) : 0;
+		if (pokemon > 0) {
+			$('#candies').val( function() {
+				return ++candies;
+			});
+			$('#pokemon').val( function() {
+				return --pokemon ? pokemon : '';
+			});
+		}
+		$('input').trigger('input');
+	});
 	$('input').on('input', function (e) {
 		$('#output').html(function () {
-			var evolutions = 0;
 			var caught_pokemon = 0;
-			var candies = ($('#candies').val().length != 0) ? parseInt($('#candies').val(), 10) : 0;
-			var pokemon_remaining = ($('#pokemon').val().length != 0) ? parseInt($('#pokemon').val(), 10) : 0;
+			var curr_evolutions = 0;
+			var curr_candies = ($('#candies').val().length != 0) ? parseInt($('#candies').val(), 10) : 0;
+			var curr_pokemon = ($('#pokemon').val().length != 0) ? parseInt($('#pokemon').val(), 10) : 0;
 			var perfect = (candies == 0 && pokemon_remaining == 0);
 			function catch_pokemon() {
 				console.log("cat", candies, pokemon_remaining);
@@ -47,19 +80,34 @@ $(function() {
 				}
 			}
 
-			do {
-				console.log("spending candies", candies, pokemon_remaining);
-				while (candies >= evolve_rate) {
+			// evolve anything we can
+			while (curr_pokemon > 0 && curr_candies >= evolve_rate) {
+				curr_evolutions++;
+				curr_candies -= evolve_rate;
+				candies++;
+				if (do_melt) {
+					candies++;
+				}
+				curr_pokemon--
+			}
+			var evolutions = curr_evolutions;
+			var candies = curr_candies;
+			var pokemon_remaining = curr_pokemon;
+
+			console.log("spending candies", candies, pokemon_remaining);
+			// first spend the candies, catch pokemon if needed
+			while (candies >= evolve_rate) {
+				evolve();
+			}
+			// transfer all pokemon and evolve if possible
+			while (pokemon_remaining > 0) {
+				melt();
+				if (candies >= evolve_rate) {
 					evolve();
 				}
-				if (pokemon_remaining > 0) {
-					melt();
-					if (candies >= evolve_rate) {
-						evolve();
-					}
-				}
-			} while (pokemon_remaining > 0);
+			}
 			console.log("determining remainder", candies, pokemon_remaining, perfect);
+			// find out how many more pokemon we need for the next checkpoint
 			if (!perfect) {
 				while (candies < evolve_rate) {
 					console.log(candies, evolve_rate);
@@ -71,13 +119,17 @@ $(function() {
 				evolutions++;
 			}
 
-			var html = "<p>Catch&nbsp;<b>"
+			var html = "<p style='font-size:25px;'>You can evolve <b>"
+			+ curr_evolutions
+			+ "</b> pokemon! <p>You will have <b>"
+			+ curr_candies
+			+ "</b> candies and <b>"
+			+ curr_pokemon
+			+ "</b>	pokemon after.</p><p>Catch&nbsp;<b>"
 			+ Math.max(caught_pokemon, 0)
-			+ "</b>&nbsp;more</p><p>for <b>"
+			+ "</b>&nbsp;more to evolve <b>"
 			+ evolutions
-			+ "</b> evolve" 
-			+ ((evolutions != 1) ? "s" : "")
-			+ "!</p>";
+			+ "</b> pokemon!</p>";
 			return html;
 		});
 	});
